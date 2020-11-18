854f11c9432dbc27d5f401587999c4b60f0573ec
Lillian Wright <10803934@my.uvu.edu>
2020-11-17 23:45:45 -0700

    gitflow-feature-stash: HandAnimations

---
+++ Growler_Repair_Sim/Assets/Scripting/HandBehavior.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.XR;
+using static UnityEngine.XR.CommonUsages;
+
+public class HandBehavior : MonoBehaviour
+{
+    public GameObject handModelPrefab;
+    public Animator handAnimator;
+
+    private InputDevice targetDevice;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //List<InputDevice> devices = new List<InputDevice>();
+        //InputDeviceCharacteristics rightControllerCharacteristics = InputDeviceCharacteristics.Right;
+        //InputDevices.GetDevicesWithCharacteristics(rightControllerCharacteristics, devices);
+
+        /*foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }*/
+        handAnimator = handModelPrefab.GetComponent<Animator>();
+    }
+
+    public void UpdateHandAnimation()
+    {
+        if (targetDevice.TryGetFeatureValue(trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
+        
+        if (targetDevice.TryGetFeatureValue(grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateHandAnimation();
+    }
+}
